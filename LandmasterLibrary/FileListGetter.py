bb8e94578d3b1e14c4a001350e5f7e52ee02b885
Landmaster3375 <52403447+masudattouteki15@users.noreply.github.com>
2020-07-17 20:44:37 +0900

    Add files via upload

--- LandmasterLibrary/FileListGetter.py
+++ LandmasterLibrary/FileListGetter.py
@@ -9,107 +9,107 @@ import DirEditor
 sep = DirEditor.DecideSeperator() # String seperator of directory.
 
 def InputExt():
-  '''
-  list_of_ext ： List String extension
-  moreFile    ： String extension
-  '''
-  list_of_ext = []
-  # Number of extensions you can selecet is under 10.
-  for i in range(10):
-    # Varify message by times.
-    if i == 0:
-      moreFile = input("What filetype do you wanna take?: ")
-    else:
-      moreFile = input('more filetype?(* or "nothing"): ')
-    # Store in a list or not.
-    if moreFile != "nothing":
-      list_of_ext.append(moreFile)
-    else:
-      break
-  return list_of_ext
+    '''
+    list_of_ext ： List String extension
+    moreFile    ： String extension
+    '''
+    list_of_ext = []
+    # Number of extensions you can selecet is under 10.
+    for i in range(10):
+        # Varify message by times.
+        if i == 0:
+            moreFile = input("What filetype do you wanna take?: ")
+        else:
+            moreFile = input('more filetype?(* or "nothing"): ')
+        # Store in a list or not.
+        if moreFile != "nothing":
+            list_of_ext.append(moreFile)
+        else:
+            break
+    return list_of_ext
 
 def DecideExt(list_of_ext):
-  '''
-  list_of_ext ： List String extension
-  fileTypes   ： List type of file for choose file in the dialog
+    '''
+    list_of_ext ： List String extension
+    fileTypes   ： List type of file for choose file in the dialog
 
-  ext          ： String extension
-  ext_by_semic ： String extension in arrangement by semicolon (if Windows)
-  ext_by_list  ： String extension in arrangement by list (if Mac)
-  ext_by_tuple ： String extension in arrangement by tuple (if Mac)
-  '''
-  # Discrimination whether Windows or Mac.
-  pf = platform.system()
-  # Select extension to choose file
-  if pf == 'Windows': # OS is Windows
-    # wanted to make like this...  ex. fileTypes = [('data files','*.pdf;*.py')]
-    ext_by_semic = ''
-    for ext in list_of_ext:
-      if ext == list_of_ext[0]:
-        ext_by_semic = '*.{ext}'.format(ext=ext)
-      else:
-        ext_by_semic = ext_by_semic + '{semicolon}*.{ext}'.format(ext=ext,semicolon=';')
-    fileTypes = [('data files', ext_by_semic)]
-  elif pf == 'Darwin': # OS is Mac
-    # wanted to make like this...  ex. fileTypes = [("csv files","*.csv"),("txt files","*.txt")]
-    for ext in list_of_ext:
-      ext_by_list = ['{ext} files'.format(ext=ext),'*.{ext}'.format(ext=ext)]
-      ext_by_tuple = tuple(ext_by_list)
-      if ext == list_of_ext[0]:
-        fileTypes = [ext_by_tuple]
-      else:
-        fileTypes.append(ext_by_tuple)
-      # set default "ext_by_tuple"
-      ext_by_tuple = ""
-  return fileTypes
+    ext          ： String extension
+    ext_by_semic ： String extension in arrangement by semicolon (if Windows)
+    ext_by_list  ： String extension in arrangement by list (if Mac)
+    ext_by_tuple ： String extension in arrangement by tuple (if Mac)
+    '''
+    # Discrimination whether Windows or Mac.
+    pf = platform.system()
+    # Select extension to choose file
+    if pf == 'Windows': # OS is Windows
+        # wanted to make like this...  ex. fileTypes = [('data files','*.pdf;*.py')]
+        ext_by_semic = ''
+        for ext in list_of_ext:
+            if ext == list_of_ext[0]:
+                ext_by_semic = '*.{ext}'.format(ext=ext)
+            else:
+                ext_by_semic = ext_by_semic + '{semicolon}*.{ext}'.format(ext=ext,semicolon=';')
+        fileTypes = [('data files', ext_by_semic)]
+    elif pf == 'Darwin': # OS is Mac
+        # wanted to make like this...  ex. fileTypes = [("csv files","*.csv"),("txt files","*.txt")]
+        for ext in list_of_ext:
+            ext_by_list = ['{ext} files'.format(ext=ext),'*.{ext}'.format(ext=ext)]
+            ext_by_tuple = tuple(ext_by_list)
+            if ext == list_of_ext[0]:
+                fileTypes = [ext_by_tuple]
+            else:
+                fileTypes.append(ext_by_tuple)
+            # set default "ext_by_tuple"
+            ext_by_tuple = ""
+    return fileTypes
 
 def DecideNowFile(list_of_ext):
-  '''
-  list_of_ext ： List String extension
-  nowDir      ： String absolutely directory of default folder
-  nowFilePath ： String absolutely path of selected file
-  '''
-  if list_of_ext == []:
-    list_of_ext = InputExt()
-  else:
-    pass
-  nowDir = os.path.abspath(os.path.dirname(__file__))
-  nowFilePath = filedialog.askopenfilename(filetypes=DecideExt(list_of_ext), initialdir=nowDir)
-  print("File's Absollutely Path: {quotation}{filepath}{quotation}".format(quotation='"',filepath=nowFilePath))
-  return nowFilePath
+    '''
+    list_of_ext ： List String extension
+    nowDir      ： String absolutely directory of default folder
+    nowFilePath ： String absolutely path of selected file
+    '''
+    if list_of_ext == []:
+        list_of_ext = InputExt()
+    else:
+        pass
+    nowDir = os.path.abspath(os.path.dirname(__file__))
+    nowFilePath = filedialog.askopenfilename(filetypes=DecideExt(list_of_ext), initialdir=nowDir)
+    print("File's Absollutely Path: {quotation}{filepath}{quotation}".format(quotation='"',filepath=nowFilePath))
+    return nowFilePath
 
 def DecideNowDir():
-  '''
-  nowDir     ： String absolutely directory default folder
-  nowDirPath ： String absolutely path selected folder
-  '''
-  nowDir = os.path.abspath(os.path.dirname(__file__))
-  nowDirPath = filedialog.askdirectory(initialdir=nowDir)
-  print("Folder's Absollutely Path: {quotation}{folderpath}{quotation}".format(quotation='"',folderpath=nowDirPath))
-  return nowDirPath
+    '''
+    nowDir     ： String absolutely directory default folder
+    nowDirPath ： String absolutely path selected folder
+    '''
+    nowDir = os.path.abspath(os.path.dirname(__file__))
+    nowDirPath = filedialog.askdirectory(initialdir=nowDir)
+    print("Folder's Absollutely Path: {quotation}{folderpath}{quotation}".format(quotation='"',folderpath=nowDirPath))
+    return nowDirPath
 
 def GetFileList(folderdir, ext):
-  '''
-  folderdir : Selected folder's absolutely directory.
-  ext        : extension
-  folderlist : list about selected folder
-  '''
+    '''
+    folderdir : Selected folder's absolutely directory.
+    ext        : extension
+    folderlist : list about selected folder
+    '''
 
-  if folderdir == '':
-    print("ERROR: No directory is selected.")
-    sys.exit(0)
+    if folderdir == '':
+        print("ERROR: No directory is selected.")
+        sys.exit(0)
 
-  folderList = glob2.glob('{folderpath}{sep}*.{ext}'.format(folderpath=folderdir,sep=sep,ext=ext))
-  # sort order of list is irregulary if you use "glob"
-  list.sort(folderList, reverse=False)
-  print('Get file list in this folder.\n"', folderdir, '"\n\n........................\n')
-  # Get list about files.
-  for file in folderList:
-    print(file)
-  return folderList
+    folderList = glob2.glob('{folderpath}{sep}*.{ext}'.format(folderpath=folderdir,sep=sep,ext=ext))
+    # sort order of list is irregulary if you use "glob"
+    list.sort(folderList, reverse=False)
+    print('Get file list in this folder.\n"', folderdir, '"\n\n........................\n')
+    # Get list about files.
+    for file in folderList:
+        print(file)
+    return folderList
 
 def main(folderdir):
-  GetFileList(folderdir)
+    GetFileList(folderdir)
 
 if __name__ == "__main__":
-  main(DirEditor.DecideNowDir())
+    main(DirEditor.DecideNowDir())
--- LandmasterLibrary/PDFRotater.py
+++ LandmasterLibrary/PDFRotater.py
@@ -8,55 +8,55 @@ sep = DirEditor.DecideSeperator() # String seperator of directory.
 import FileListGetter
 
 def MakeVertical(folderList):
-  '''
-  folderList        : list of file filtered with extension in the selected folder.
-  filename          : fullname of selected file.
-  original          : PdfFileReader of original files.
-  rotated           : PdfFileWriter of rotated files.
-  rotated_dir       : path of folder having rotated files.
-  sum_page          : sum of page.
-  sum_rotating_page : rotating pages. count 1 if make their vertical.
-  '''
-  # PyPDF2 version is 1.26.0
-  from PyPDF2 import PdfFileWriter, PdfFileReader
-  if len(folderList) != 0:
-    filename = folderList[0]
-  else:
-    print('\nPDFRotater exits because of no target files.')
-    sys.exit(0)
+    '''
+    folderList        : list of file filtered with extension in the selected folder.
+    filename          : fullname of selected file.
+    original          : PdfFileReader of original files.
+    rotated           : PdfFileWriter of rotated files.
+    rotated_dir       : path of folder having rotated files.
+    sum_page          : sum of page.
+    sum_rotating_page : rotating pages. count 1 if make their vertical.
+    '''
+    # PyPDF2 version is 1.26.0
+    from PyPDF2 import PdfFileWriter, PdfFileReader
+    if len(folderList) != 0:
+        filename = folderList[0]
+    else:
+        print('\nPDFRotater exits because of no target files.')
+        sys.exit(0)
 
-  rotated_dir = DirEditor.MakeDirectory(filename)
+    rotated_dir = DirEditor.MakeDirectory(filename)
 
-  for filename in folderList:
-    print("Rotate all page of PDF:", filename)
+    for filename in folderList:
+        print("Rotate all page of PDF:", filename)
 
-    original = PdfFileReader(filename)
-    rotated  = PdfFileWriter()
+        original = PdfFileReader(filename)
+        rotated  = PdfFileWriter()
 
-    # Display number of pages of original PDF file
-    sum_page = original.getNumPages()
-    print("page_num: ", sum_page)
+        # Display number of pages of original PDF file
+        sum_page = original.getNumPages()
+        print("page_num: ", sum_page)
 
-    # Count pages at the same time as making vertical.
-    sum_rotating_page = 0
-    for i in range(0, sum_page, 1):
-      print("Page ", i, " is ", original.getPage(i).get('/Rotate'), " degrees rotating")
-      angle_rotating = original.getPage(i).get('/Rotate')
-      if angle_rotating != 0:
-        sum_rotating_page += 1
-      rotated.addPage(original.getPage(i).rotateClockwise(360 - angle_rotating))
+        # Count pages at the same time as making vertical.
+        sum_rotating_page = 0
+        for i in range(0, sum_page, 1):
+            print("Page ", i, " is ", original.getPage(i).get('/Rotate'), " degrees rotating")
+            angle_rotating = original.getPage(i).get('/Rotate')
+            if angle_rotating != 0:
+                sum_rotating_page += 1
+            rotated.addPage(original.getPage(i).rotateClockwise(360 - angle_rotating))
 
-    # new file entry
-    output_filename = '{dirname}{sep}{basename}'.format(dirname=rotated_dir,sep=sep,basename=os.path.basename(filename))
-    with open(output_filename, "wb") as outputStream:
-      rotated.write(outputStream)
+        # new file entry
+        output_filename = '{dirname}{sep}{basename}'.format(dirname=rotated_dir,sep=sep,basename=os.path.basename(filename))
+        with open(output_filename, "wb") as outputStream:
+            rotated.write(outputStream)
 
-    print("\nPDFRotater is terminated.\nsum_rotating_page: ", sum_rotating_page, " / ", sum_page)
+        print("\nPDFRotater is terminated.\nsum_rotating_page: ", sum_rotating_page, " / ", sum_page)
 
-  print('\n\nCheck new folder. "{}"'.format(rotated_dir))
+    print('\n\nCheck new folder. "{}"'.format(rotated_dir))
 
 def main(folderpath):
-  MakeVertical(FileListGetter.GetFileList(FileListGetter.DecideNowDir(),'pdf'))
+    MakeVertical(FileListGetter.GetFileList(FileListGetter.DecideNowDir(),'pdf'))
 
 if __name__ == "__main__":
-  main()
+    main()
