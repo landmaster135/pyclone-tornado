6d1deed27fc5740a22cef4546a0275aeba295a6c
kinkinbeer135ml <52403447+Landmaster135@users.noreply.github.com>
2021-11-21 22:13:13 +0900

    Add files via upload

--- LandmasterLibrary/test/test_TextReplacer.py
+++ LandmasterLibrary/test/test_TextReplacer.py
@@ -1,27 +1,57 @@
 from TextReplacer import ReplaceCharacter
 
 class Test_ReplaceCharacter:
-    def test_MakeVoicedsound_1(self):
+    # Unicode : \u3099
+    def test_MakeVoicedsound_1_1(self):
         replaceCharacter = ReplaceCharacter()
         assert replaceCharacter.MakeVoicedsound('がぎぐげござじずぜぞ') == 'がぎぐげござじずぜぞ'
 
-    def test_MakeVoicedsound_2(self):
+    def test_MakeVoicedsound_1_2(self):
         replaceCharacter = ReplaceCharacter()
         assert replaceCharacter.MakeVoicedsound('だぢづでどばびぶべぼ') == 'だぢづてどばびぶべぼ'
 
-    def test_MakeVoicedsound_3(self):
+    def test_MakeVoicedsound_1_3(self):
         replaceCharacter = ReplaceCharacter()
         assert replaceCharacter.MakeVoicedsound('ぱぴぷぺぽ') == 'ぱぴぷぺぽ'
 
-    def test_MakeVoicedsound_4(self):
+    def test_MakeVoicedsound_1_4(self):
+        replaceCharacter = ReplaceCharacter()
+        assert replaceCharacter.MakeVoicedsound('ガギグゲゴザジズゼゾ') == 'ガギグゲゴザジズゼゾ'
+
+    def test_MakeVoicedsound_1_5(self):
+        replaceCharacter = ReplaceCharacter()
+        assert replaceCharacter.MakeVoicedsound('ダヂヅデドバビブベボ') == 'ダヂヅデドバビブベボ'
+
+    # Unicode : \u309A
+    def test_MakeVoicedsound_1_6(self):
+        replaceCharacter = ReplaceCharacter()
+        assert replaceCharacter.MakeVoicedsound('パピプペポ') == 'パピプペポ'
+
+    # dakuon
+    def test_MakeVoicedsound_2_1(self):
         replaceCharacter = ReplaceCharacter()
         assert replaceCharacter.MakeVoicedsound('がぎぐげござじずぜぞ') == 'がぎぐげござじずぜぞ'
 
-    def test_MakeVoicedsound_5(self):
+    def test_MakeVoicedsound_2_2(self):
         replaceCharacter = ReplaceCharacter()
         assert replaceCharacter.MakeVoicedsound('だぢづてどばびぶべぼ') == 'だぢづてどばびぶべぼ'
 
-    def test_MakeVoicedsound_6(self):
+    #handakuon
+    def test_MakeVoicedsound_2_3(self):
         replaceCharacter = ReplaceCharacter()
         assert replaceCharacter.MakeVoicedsound('ぱぴぷぺぽ') == 'ぱぴぷぺぽ'
+
+    # dakuon
+    def test_MakeVoicedsound_2_4(self):
+        replaceCharacter = ReplaceCharacter()
+        assert replaceCharacter.MakeVoicedsound('ガギグゲゴザジズゼゾ') == 'ガギグゲゴザジズゼゾ'
+
+    def test_MakeVoicedsound_2_5(self):
+        replaceCharacter = ReplaceCharacter()
+        assert replaceCharacter.MakeVoicedsound('ダヂヅデドバビブベボ') == 'ダヂヅデドバビブベボ'
+
+    # handakuon
+    def test_MakeVoicedsound_2_6(self):
+        replaceCharacter = ReplaceCharacter()
+        assert replaceCharacter.MakeVoicedsound('パピプペポ') == 'パピプペポ'
 
